459ead3e32951b5c8f53f397fdc8823c5ba415cf
Ed Salter <edwardcsalter@gmail.com>
2014-08-26 21:23:19 -0400

    Added NSLogs & background color
    -NSLog’d Start, Touch, Delta, and Origin of both x & y of DragView class
    -Made background color of DragView white to show bounds of said class

--- DragView/DragView/DragView.m
+++ DragView/DragView/DragView.m
@@ -17,24 +17,34 @@
     self = [super initWithImage:image];
     if (self) {
         [self setUserInteractionEnabled:true];
+        //White background shows bounds of dragView
+        self.backgroundColor = [UIColor whiteColor];
     }
     return self;
 }
 
+#pragma mark TOUCHES_BEGAN
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     startCoord = [[touches anyObject]locationInView:self];
     [self.superview bringSubviewToFront:self];
 }
 
+#pragma mark TOUCHES_MOVED
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     CGPoint point = [[touches anyObject]locationInView:self];
     float dx = point.x - startCoord.x;
+    
+    NSLog(@"Start.x: %f", startCoord.x);
     NSLog(@"Touch.x: %f",point.x);
     NSLog(@"Delta.x: %f",dx);
-    
+    NSLog(@"Origin.x: %f", self.bounds.origin.x);
+
     float dy = point.y - startCoord.y;
+    
+    NSLog(@"Start.y: %f", startCoord.y);
     NSLog(@"Touch.y: %f",point.y);
     NSLog(@"Delta.y: %f",dy);
+    NSLog(@"Origin.y: %f", self.bounds.origin.y);
     
     CGPoint centerNew = CGPointMake(self.center.x + dx, self.center.y + dy);
     self.center = centerNew;
